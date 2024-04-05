43ebd8ab79e29d90f8e987a1c7aea017104b66b7
Ritika Sahay <113190073+SahayRitika@users.noreply.github.com>
2024-04-05 10:09:03 +0530

    Create Leetcode 1544. Make The String Great.java
    StringBuilder costs less time than iteratively making the string(from stack).

---
+++ Leetcode 1544. Make The String Great.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack= new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(stack.isEmpty()){
+                stack.push(ch);
+                continue;
+            }
+            if ((Character.isUpperCase(ch) && Character.isLowerCase(stack.peek())
+                    && Character.toUpperCase(stack.peek()) == ch) ||
+                    Character.isUpperCase(stack.peek()) && Character.isLowerCase(ch)
+                            && Character.toUpperCase(ch) == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder ans= new StringBuilder();
+        for(char ch:stack){
+            ans.append(ch);
+        }
+        return ans.toString();
+    }
+}
